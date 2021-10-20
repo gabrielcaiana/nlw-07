47cec502c8c6c69e563b45c2823520e43a0f6151
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-20 20:40:29 -0300

    recuperando o access_token

--- back-end/src/services/authenticateUserService.ts
+++ back-end/src/services/authenticateUserService.ts
@@ -1,10 +1,14 @@
 import axios from "axios"
 
+interface IAccessTokenResponse {
+  access_token: string
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = `http://github.com/login/oauth/access_token`
 
-    const response = await axios.post(url, null, {
+    const { data: IAccessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id : process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -15,7 +19,7 @@ class AuthenticateUserService {
       }
     })
 
-    return response.data
+    return IAccessTokenResponse.access_token
   }
 }
 
