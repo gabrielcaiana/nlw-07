e099825373f708472c6f8798ec38fece393e53d4
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-23 12:28:32 -0300

    list 3 last messages

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 import { CreateMessageController } from "./controllers/CreateMessageController"
+import { Get3LastMessageController } from "./controllers/Get3LastMessageController"
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated"
+import { Get3LastMessageService } from "./services/Get3LastMessageService"
 
 const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
 
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
+
+router.get("/messages/last3", new Get3LastMessageController().handle)
 
 export { router }
