e07c262de6da8a42c9b71f6c9452d0a3e9426e7a
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-24 12:51:09 -0300

    finish style

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,5 +1,61 @@
+import styles from './messageListWrapper.module.scss';
+
+import logo from '../../assets/logo.svg';
+
 export function MessageList() {
-  return(
-    <h1>MessageList</h1>
-  )
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logo} alt="doWhile" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/gabrielcaiana.png"
+                alt="Gabriel Caiana"
+              />
+            </div>
+            <span>Gabriel Caiana</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/gabrielcaiana.png"
+                alt="Gabriel Caiana"
+              />
+            </div>
+            <span>Gabriel Caiana</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/gabrielcaiana.png"
+                alt="Gabriel Caiana"
+              />
+            </div>
+            <span>Gabriel Caiana</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
