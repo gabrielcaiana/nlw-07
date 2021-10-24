334719d62a2b2084570732bc6b22ba577a693e3a
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-24 14:36:47 -0300

    list messages

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,60 +1,53 @@
-import styles from './messageListWrapper.module.scss';
+import { api } from '../../services/api';
 
+import styles from './messageListWrapper.module.scss';
 import logo from '../../assets/logo.svg';
 
+import { useEffect, useState } from 'react';
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last3').then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  console.log(messages);
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logo} alt="doWhile" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/gabrielcaiana.png"
-                alt="Gabriel Caiana"
-              />
-            </div>
-            <span>Gabriel Caiana</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/gabrielcaiana.png"
-                alt="Gabriel Caiana"
-              />
-            </div>
-            <span>Gabriel Caiana</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/gabrielcaiana.png"
-                alt="Gabriel Caiana"
-              />
-            </div>
-            <span>Gabriel Caiana</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li className={styles.message}>
+              <p className={styles.messageContent}>
+                {message.text}
+              </p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img
+                    src={message.user.avatar_url}
+                    alt={message.user.name}
+                  />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
